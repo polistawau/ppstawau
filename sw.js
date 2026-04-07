724db9201978ffed3e0336ba06966b9c11b361e6
polistawau <polistawau@gmail.com>
2026-04-07 20:26:13 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))));self.clients.claim();});
