76bfa8d29b23529eabc21db3df5fe090375b043c
polistawau <polistawau@gmail.com>
2026-04-08 01:38:11 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))));self.clients.claim();});
